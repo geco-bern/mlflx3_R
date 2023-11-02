a21bf4aa44a3c955c7ba38e4db89d9f37d4628f2
Koen Hufkens <koen.hufkens@gmail.com>
2023-11-02 19:42:07 +0100

    remove scraps

--- analysis/scraps/scraps.R
+++
@@ -1,7 +0,0 @@
-# forward index to subset data based upon
-# index, optionally sample the sites further
-# to leave a fraction of the sites
-self$idx <- sort(sample.int(
-  n = length(self$sites),
-  size = length(self$sites) * sample_frac
-))
